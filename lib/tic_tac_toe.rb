3e97e8f1c841c57da5107e19572d8c4dad5abbbb
Jacob Borga <jdborga@gmail.com>
2021-03-05 02:34:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,6 +69,66 @@ class TicTacToe
   end
 
   def won?
+  WIN_COMBINATIONS.each do |combo|
+    win_index_1 = combo[0]
+    win_index_2 = combo[1]
+    win_index_3 = combo[2]
 
+    position_1 = @board[win_index_1]
+    position_2 = @board[win_index_2]
+    position_3 = @board[win_index_3]
+
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      return combo
+    end
   end
+  return false
+end
+
+# Determines if the board is in a full state
+def full?
+  if @board.include?(" ") || @board.include?("")
+    return false
+  end
+  return true
+end
+
+def draw?
+  if full? && !won?
+    return true
+  end
+  return false
+end
+
+# Determines if the game is over based on the boards state
+def over?
+  if won? || draw?
+    return true
+  end
+  return false
+end
+
+# Determines if there is a winner based on the boards state
+def winner
+  win_combo = won?
+  if !win_combo
+    return nil
+  elsif (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X")
+    return "X"
+  elsif (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
+    return "O"
+  end
+end
+
+def play
+  until over? do
+    turn
+  end
+  if draw?
+    puts "Cat's Game!"
+  else
+    win = winner
+    puts "Congratulations #{win}!"
+  end
+end
 end
