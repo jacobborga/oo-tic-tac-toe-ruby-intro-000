68102cce14b588530cc6e1fdff5fe186ce7f2078
Jacob Borga <jdborga@gmail.com>
2021-03-05 02:29:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,7 @@ class TicTacToe
 
   def turn
     puts "Please enter a number 1-9: "
-    @user_input = gets.chomp
+    user_input = gets.chomp
     @user_input = input_to_index(user_input)
     current = current_player(board)
     if valid_move?(board, user_input)
